0ab0a2a2ed0d28c957607171620d7d781a9a22c2
macblanelw <macblanelw@gmail.com>
2021-12-11 23:48:40 -0500

    Through useStateHook video

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,33 @@
 import React from 'react'
-
-
-const HandleNames = () => {
-  const names = ['Allen', 'Bob', 'Charlie', 'Dick'];
-  const int = Math.floor(Math.random() * names.length);
-  return names[int];
-}
-
-const handleClick = () => {
-    console.log("Clicked!")
-}
-
-const handleClick2 = (name) => {
-    console.log(`${name} was Clicked!`)
-}
-
-const handleClick3 = (e) => {
-    console.log(e.target.innerText)
-}
+import { useState } from 'react';
 
 const Content = () => {
+    const [name, setName] = useState('Lance');
+    const [count, setCount] = useState(0);
+    
+    const handleNameChange = () => {
+        const names = ['Allen', 'Bob', 'Charlie', 'Dick', 'Lance'];
+        const int = Math.floor(Math.random() * names.length);
+        setName(names[int]);
+    }
+
+    const handleClick = () => {
+        setCount(count + 1)
+        console.log(count)
+    }
+
+    const handleClick2 = () => {
+        console.log(count)
+    }
+
     return (
         <main>
-            <p onDoubleClick={handleClick}>You are such a {HandleNames()}!</p>    
-            <button onClick={ handleClick }>Click it!</button>
-            <button onClick={ () => handleClick2('Lance') }>Click it!</button>        
-            <button onClick={ (e) => handleClick3(e) }>Click it!</button>        
+            <p>
+                You are such a {name}!
+            </p>
+            <button onClick={ handleNameChange }>Change Name</button>
+            <button onClick={ handleClick }>Click It!</button>
+            <button onClick={ handleClick2 }>Click It!</button>
         </main>
     )
 }
